07a511193fb98ca544b4722dd4aca80cba2cc281
Beyda Soyseven <beydasoyseven@outlook.com>
2022-04-30 18:25:46 +0300

    hata düzeltme
    deneme

--- app/src/main/java/com/example/hasatimagineproj/MainActivity.java
+++ app/src/main/java/com/example/hasatimagineproj/MainActivity.java
@@ -21,7 +21,6 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AgricultSignInActivity.class));
             }
         });
-
         findViewById(R.id.customerButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
