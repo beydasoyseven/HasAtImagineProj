1769ca5e5fbdfdc1f8e15e1eaeb99a1c37ba7f81
Beyda Soyseven <81165674+beydasoyseven@users.noreply.github.com>
2022-04-30 18:32:20 +0300

    Revert "hata düzeltme"
    This reverts commit 07a511193fb98ca544b4722dd4aca80cba2cc281.

--- app/src/main/java/com/example/hasatimagineproj/MainActivity.java
+++ app/src/main/java/com/example/hasatimagineproj/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, AgricultSignInActivity.class));
             }
         });
+
         findViewById(R.id.customerButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
